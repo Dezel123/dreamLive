0caf834b7333b13aedd15069ed131e827cbfab31
Dezel123 <55340329+Dezel123@users.noreply.github.com>
2019-09-16 06:15:05 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,7 +8,7 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : DREAM ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`How to be Fury`,"http://twitch.tv/Dream")
+client.user.setGame(`Hacking The System`,"http://twitch.tv/Dream")
 client.user.setStatus("dnd")
 });
 
